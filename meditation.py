9f267c315c9fb589016f439ddaa12564533dfa5a
Tesfa Yohannes <tmyohannes@gmail.com>
2023-10-24 00:06:58 +0800

    audio only merged post

--- meditation.py
+++ meditation.py
@@ -36,13 +36,11 @@ def txt_to_img(text):
 
     return gr.Image(response)
 
-def generate_thumbnail(audio_gr, generated_video, img_result):
+def generate_thumbnail_rq(audio_gr, generated_video, img_result):
     response = generate_thumbnail(audio_gr, generated_video, img_result)
 
     # currently returning the file name
     return response
-
-
 
 with gr.Blocks(theme="gradio/monochrome") as demo:
     # later on i want it to look like a mobile app with entire application centred. 
@@ -60,7 +58,7 @@ with gr.Blocks(theme="gradio/monochrome") as demo:
             dropdown = gr.Dropdown(label="Speaker style", choices=["p336", "p339", "p326"])
             generate.click(fn=generate_affirmation, inputs=[text], outputs=[text_gen])
 
-            audio_gr = gr.Audio(label="Generated Audio")
+            audio_gr = gr.Audio(label="Generated Audio", type="filepath")
             generate_tts = gr.Button("Generate TTS Audio", label="Generate TTS Audio")
             generate_tts.click(fn=generate_tts_fn, inputs=[text_gen, dropdown], outputs=[audio_gr])
 
@@ -84,7 +82,7 @@ with gr.Blocks(theme="gradio/monochrome") as demo:
 
             merged_audio = gr.Audio(label="Merged Audio")
 
-            combine_all.click(fn=generate_thumbnail, inputs=[audio_gr, generated_video, img_result], outputs=[merged_audio])
+            combine_all.click(fn=generate_thumbnail_rq, inputs=[audio_gr, generated_video, img_result], outputs=[merged_audio])
 
 if __name__ == "__main__":
     print("Starting Gradio Server")
